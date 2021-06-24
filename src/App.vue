63666f09a21370183a45d4aa05614fd3c50a1eb3
Nan Luo <1410314963@qq.com>
2021-06-24 11:09:34 +0800

    处理样式

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,34 @@
   <div id="app">
     <!--根路由出口-->
     <h1>Hello Vue</h1>
-    <router-view/>
+    <p>{{ name }}</p>
+    <router-view />
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type='primary'>主要按钮</el-button>
+    </el-row>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
 
-<style lang="scss" scoped></style>
+@Component
+export default class App extends Vue {
+  count = 2;
+  name = 'iPhone';
+  prop = {
+    color: 'black',
+    type: '11'
+  };
+
+  test () {
+    console.log(this.name)
+  }
+}
+</script>
+<style lang="scss" scoped>
+p {
+  color: $danger-color;
+}
+</style>
